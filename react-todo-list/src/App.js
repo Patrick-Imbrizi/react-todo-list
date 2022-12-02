6c01cbd5c337c47caaecbb25f829930c88dee4d6
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2022-12-01 21:50:55 -0300

    init

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,23 +1,17 @@
+import react from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
